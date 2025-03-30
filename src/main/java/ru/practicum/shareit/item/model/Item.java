7b1618792bbdf7f33b80de6a787a3a7d2484452b
Valerii_Butko <lb2005lba@gmail.com>
2025-03-30 15:14:29 +0500

    Save my local changes

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,15 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import ru.practicum.shareit.user.model.User;
-
-@Data
-@NoArgsConstructor
+/**
+ * TODO Sprint add-controllers.
+ */
 public class Item {
-    private Long id;
-    private String name;
-    private String description;
-    private User owner;
-    private ItemStatus status;
 }
--- src/main/java/ru/practicum/shareit/item/model/ItemStatus.java
+++
@@ -1,5 +0,0 @@
-package ru.practicum.shareit.item.model;
-
-public enum ItemStatus {
-    AVAILABLE, UNAVAILABLE
-}
