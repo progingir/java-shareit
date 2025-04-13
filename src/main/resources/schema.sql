5573dd3ec619884ca0d9fabb4dac35c0dbe842a7
Valerii_Butko <lb2005lba@gmail.com>
2025-04-13 16:45:54 +0500

    добавили foreign key

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS items (
     is_available BOOLEAN NOT NULL,
     owner_id BIGINT NOT NULL,
     request_id BIGINT,
-    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (request_id) REFERENCES requests(id) ON DELETE CASCADE
 );
 
 -- Таблица бронирований
