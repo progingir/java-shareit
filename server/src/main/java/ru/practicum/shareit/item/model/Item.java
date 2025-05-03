6a7a4d0ae73f1bf2c32db797aa4bb5bc369b7462
Valerii_Butko <lb2005lba@gmail.com>
2025-05-04 04:38:11 +0500

    все еще пытаюсь

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,34 +1,34 @@
 package ru.practicum.shareit.item.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
-@Data
-@NoArgsConstructor
 @Entity
 @Table(name = "items")
+@Getter
+@Setter
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(nullable = false)
     private String name;
 
-    @Column(name = "description", nullable = false)
+    @Column
     private String description;
+
+    @Column(name = "is_available", nullable = false)
+    private Boolean available;
 
     @ManyToOne
     @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
 
-    @Column(name = "is_available", nullable = false)
-    private boolean available = false;
-
     @ManyToOne
-    @JoinColumn(name = "request_id")
+    @JoinColumn(name = "item_request_id")
     private ItemRequest itemRequest;
 }
