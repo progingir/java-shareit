f312afd084953e4ed173afcc318daea1fb18b35d
Valerii_Butko <lb2005lba@gmail.com>
2025-05-04 02:42:32 +0500

    откат изменений

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -14,9 +14,9 @@ public class ItemRequestDto {
     @NotBlank(message = "Описание запроса не может быть пустым")
     private String description;
 
-    private Requester requester;
+    private Long requestorId;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime created;
 
     private List<ItemDto> items;
@@ -25,23 +25,6 @@ public class ItemRequestDto {
     public static class ItemDto {
         private Long id;
         private String name;
-        private String description;
-        private Boolean available;
-        private Owner owner;
-        private Long requestId;
-    }
-
-    @Data
-    public static class Requester {
-        private Long id;
-        private String email;
-        private String name;
-    }
-
-    @Data
-    public static class Owner {
-        private Long id;
-        private String email;
-        private String name;
+        private Long ownerId;
     }
 }
