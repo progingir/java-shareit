a0a2013edc1e1dc3bb3b0943b4985629c38f94e4
Valerii_Butko <lb2005lba@gmail.com>
2025-05-04 02:48:23 +0500

    ???

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -14,6 +15,7 @@ public class ItemRequestDto {
     @NotBlank(message = "Описание запроса не может быть пустым")
     private String description;
 
+    @JsonProperty("requester")
     private Requester requester;
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
