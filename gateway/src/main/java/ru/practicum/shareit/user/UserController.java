64459a389cbd375e72a7214eb43989f1a514ace2
Valerii_Butko <lb2005lba@gmail.com>
2025-05-05 02:27:59 +0500

    убрала все логи

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -13,32 +12,27 @@ import ru.practicum.shareit.user.dto.UpdateUserRequest;
 @Controller
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
-@Slf4j
 @Validated
 public class UserController {
     private final UserClient userClient;
 
     @GetMapping("/{userId}")
     public ResponseEntity<Object> getUserById(@PathVariable long userId) {
-        //log.info("Request to get user with ID {} received.", userId);
         return userClient.getUserById(userId);
     }
 
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody @Valid NewUserRequest userRequestDto) {
-        //log.info("Request to create new user received: {}", userRequestDto);
         return userClient.addUser(userRequestDto);
     }
 
     @PatchMapping("/{userId}")
     public ResponseEntity<Object> update(@PathVariable long userId, @RequestBody @Valid UpdateUserRequest userRequestDto) {
-        //log.info("Request to update user received: {}", userRequestDto);
         return userClient.updateUser(userId, userRequestDto);
     }
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUser(@PathVariable long userId) {
-        //log.info("Request to delete user with ID {} received.", userId);
         return userClient.deleteUser(userId);
     }
 }
